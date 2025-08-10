ce53a7b3c6217f724b48422764e9159d0fe24bd1
Andres Bravo <bravoandres1706@gmail.com>
2025-08-09 20:25:58 -0400

    header dashboard information in process

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App.tsx";
 import "./assets/styles/designBackground.css";
 import "./assets/styles/stylesUsers/styleLogin.css";
 import "./assets/styles/stylesDashboard/headerDashboardStyle.css";
+import "./assets/styles/stylesDashboard/designInformationLevelStyle.css";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
--- src/pages/Games/Presentation/Dashboard.tsx
+++ src/pages/Games/Presentation/Dashboard.tsx
@@ -4,29 +4,84 @@ import profileIcon from "../../../assets/filesSvg/filesdashboardSvg/profile.svg"
 const Dashboard = () => {
     useAuthRedirect(); // Custom hook to handle redirection if not authenticated
     return (
-        <div className="header-dashboard-container">
-            <div className="item-header-dashboard">
-                <svg
-                    className="dashboard-icon-fire"
-                    viewBox="415 411 26 32"
-                    fill="#FF7C02"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <path
-                        d="M428.5,441 C422.148,441 417,435.641 417,429.625 C417,428.228 417.031,427.094 418,426 C417.895,426.634 419.397,432.055 424.305,431.771 C424.092,427.652 422.978,417.561 428.152,414.073 C427.695,419.557 429.038,426.924 435.029,428 C434.686,425.801 434.727,422.143 436.267,421.467 C436.433,424.836 438.924,426.914 438.924,430.152 C438.924,436.016 433.251,441 428.5,441 L428.5,441 Z M437.905,417.953 C433.52,419.203 432.717,422.748 433,425 C429.872,421.322 430,417.093 430,411 C419.968,414.783 422.301,425.688 422,429 C419.477,426.935 419,422 419,422 C416.336,423.371 415,427.031 415,430 C415,437.18 420.82,443 428,443 C435.18,443 441,437.18 441,430 C441,425.733 437.867,423.765 437.905,417.953 L437.905,417.953 Z"
+        <>
+            <div className="header-dashboard-container">
+                <div className="item-header-dashboard">
+                    <svg
+                        className="dashboard-icon-fire"
+                        viewBox="415 411 26 32"
                         fill="#FF7C02"
-                    />
-                </svg>
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            d="M428.5,441 C422.148,441 417,435.641 417,429.625 C417,428.228 417.031,427.094 418,426 C417.895,426.634 419.397,432.055 424.305,431.771 C424.092,427.652 422.978,417.561 428.152,414.073 C427.695,419.557 429.038,426.924 435.029,428 C434.686,425.801 434.727,422.143 436.267,421.467 C436.433,424.836 438.924,426.914 438.924,430.152 C438.924,436.016 433.251,441 428.5,441 L428.5,441 Z M437.905,417.953 C433.52,419.203 432.717,422.748 433,425 C429.872,421.322 430,417.093 430,411 C419.968,414.783 422.301,425.688 422,429 C419.477,426.935 419,422 419,422 C416.336,423.371 415,427.031 415,430 C415,437.18 420.82,443 428,443 C435.18,443 441,437.18 441,430 C441,425.733 437.867,423.765 437.905,417.953 L437.905,417.953 Z"
+                            fill="#FF7C02"
+                        />
+                    </svg>
+                </div>
+                <div className="item-header-dashboard">500 pts</div>
+                <div className="item-header-dashboard-profile">
+                    <img src={profileIcon} alt="Profile Icon" />
+                </div>
             </div>
-            <div className="item-header-dashboard">500 pts</div>
-            <div className="item-header-dashboard-profile">
-                <img
-                    src={profileIcon}
-                    alt="Profile Icon"
-                    // style={{ width: "50px", height: "50px" }}
-                />
+            <div className="information-design-level-style-container-grid">
+                <div className="item-information-design-level-style-1">
+                    <h1 className="design-font-information-1">Etapa 1, Seccion 1</h1>
+                    <h1 className="design-font-information-2">Diagramas de flujo</h1>
+                </div>
+                <div className="item-information-design-level-style-2">
+                    <svg
+                        className="item-information-design"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            d="M21 16C21 18.8284 21 20.2426 20.1213 21.1213C19.2426 22 17.8284 22 15 22H9C6.17157 22 4.75736 22 3.87868 21.1213C3 20.2426 3 18.8284 3 16V8C3 5.17157 3 3.75736 3.87868 2.87868C4.75736 2 6.17157 2 9 2H15C17.8284 2 19.2426 2 20.1213 2.87868C21 3.75736 21 5.17157 21 8V12"
+                            stroke="#000000"
+                            strokeWidth="1.5"
+                            strokeLinecap="round"
+                        />
+                        <path
+                            d="M8 2V6M8 22V10"
+                            stroke="#000000"
+                            strokeWidth="1.5"
+                            strokeLinecap="round"
+                        />
+                        <path
+                            d="M2 12H4"
+                            stroke="#000000"
+                            strokeWidth="1.5"
+                            strokeLinecap="round"
+                        />
+                        <path
+                            d="M2 16H4"
+                            stroke="#000000"
+                            strokeWidth="1.5"
+                            strokeLinecap="round"
+                        />
+                        <path
+                            d="M2 8H4"
+                            stroke="#000000"
+                            strokeWidth="1.5"
+                            strokeLinecap="round"
+                        />
+                        <path
+                            d="M11.5 6.5H16.5"
+                            stroke="#000000"
+                            strokeWidth="1.5"
+                            strokeLinecap="round"
+                        />
+                        <path
+                            d="M11.5 10H16.5"
+                            stroke="#000000"
+                            strokeWidth="1.5"
+                            strokeLinecap="round"
+                        />
+                    </svg>
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 
