5e7dc1e513bf1845ee13249c380e39375ae00653
Andres Bravo <bravoandres1706@gmail.com>
2025-08-20 15:54:25 -0400

    Revert "bad"
    This reverts commit 0fe8d04cfb8ae0e5fc84513c325b17f6117c3586.

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@ import { TokenProvider } from "./Context/TokenProvider";
 import Login from "./pages/users/Login";
 import SignUp from "./pages/users/SignUp";
 import Dashboard from "./pages/Games/Presentation/Dashboard";
-import Exercise1 from "./pages/Games/Level1/bigScreen/Exercise1";
-import Exercise1Small from "./pages/Games/Level1/smallScreen/Exercise1Small";
+import Exercise1 from "./pages/Games/Level1/Exercise1";
 
 function App() {
     return (
@@ -16,10 +15,6 @@ function App() {
                         <Route path="/registro" element={<SignUp />} />
                         <Route path="/dashboard" element={<Dashboard />} />
                         <Route path="/exercise1" element={<Exercise1 />} />
-                        <Route
-                            path="/exercise1-small"
-                            element={<Exercise1Small />}
-                        />
                     </Routes>
                 </BrowserRouter>
             </TokenProvider>
--- src/pages/Games/Level1/smallScreen/Exercise1Small.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Exercise1Small = () => {
-    return <div>Exercise1Small</div>;
-};
-
-export default Exercise1Small;
