6b719e49175e68ee4a2fac10efa401c12ccecf1c
Andres Bravo <bravoandres1706@gmail.com>
2025-08-31 20:34:22 -0400

    optimizado el recorrido de imagenes

--- src/pages/Games/Level1/Exercise1.tsx
+++ src/pages/Games/Level1/Exercise1.tsx
@@ -6,29 +6,31 @@ import "react-resizable/css/styles.css";
 import { useNavigate } from "react-router-dom";
 import useIsMobile from "../../../hooks/useIsMobile";
 
-interface Widget {
-    id: number;
-    content: string;
-}
+import { initialWidgets } from "./widgetsDataLevel1";
 
-const initialWidgets: Widget[] = [
-    { id: 1, content: "https://i.imgur.com/yxr4QMR.png" },
-    { id: 2, content: "https://i.imgur.com/2n11hJn.png" },
-    { id: 3, content: "https://i.imgur.com/DtUtPZK.png" },
-    { id: 4, content: "https://i.imgur.com/YmsczfQ.png" },
-    { id: 5, content: "https://i.imgur.com/hgPQ06G.png" },
-    { id: 6, content: "https://i.imgur.com/oRp6iv2.png" },
-    { id: 7, content: "https://i.imgur.com/BUGBEOc.png" },
-    { id: 8, content: "https://i.imgur.com/hNUaZS0.jpeg" },
-    { id: 9, content: "https://i.imgur.com/hNUaZS0.jpeg" },
-    { id: 10, content: "https://i.imgur.com/hNUaZS0.jpeg" },
-    { id: 11, content: "https://i.imgur.com/Now9q9e.png" },
-    { id: 12, content: "https://i.imgur.com/iuyJU46.png" },
-    { id: 13, content: "https://i.imgur.com/9as8FXr.png" },
-    { id: 14, content: "https://i.imgur.com/BUGBEOc.png" },
-    { id: 15, content: "https://i.imgur.com/BUGBEOc.png" },
-    { id: 16, content: "https://i.imgur.com/BUGBEOc.png" },
-];
+// interface Widget {
+//     id: number;
+//     content: string;
+// }
+
+// const initialWidgets: Widget[] = [
+//     { id: 1, content: "https://i.imgur.com/yxr4QMR.png" },
+//     { id: 2, content: "https://i.imgur.com/2n11hJn.png" },
+//     { id: 3, content: "https://i.imgur.com/DtUtPZK.png" },
+//     { id: 4, content: "https://i.imgur.com/YmsczfQ.png" },
+//     { id: 5, content: "https://i.imgur.com/hgPQ06G.png" },
+//     { id: 6, content: "https://i.imgur.com/oRp6iv2.png" },
+//     { id: 7, content: "https://i.imgur.com/BUGBEOc.png" },
+//     { id: 8, content: "https://i.imgur.com/hNUaZS0.jpeg" },
+//     { id: 9, content: "https://i.imgur.com/hNUaZS0.jpeg" },
+//     { id: 10, content: "https://i.imgur.com/hNUaZS0.jpeg" },
+//     { id: 11, content: "https://i.imgur.com/Now9q9e.png" },
+//     { id: 12, content: "https://i.imgur.com/iuyJU46.png" },
+//     { id: 13, content: "https://i.imgur.com/9as8FXr.png" },
+//     { id: 14, content: "https://i.imgur.com/BUGBEOc.png" },
+//     { id: 15, content: "https://i.imgur.com/BUGBEOc.png" },
+//     { id: 16, content: "https://i.imgur.com/BUGBEOc.png" },
+// ];
 
 const GRID_COLS = 10;
 const GRID_ROWS = 10;
---
+++ src/pages/Games/Level1/widgetsDataLevel1.ts
@@ -0,0 +1,20 @@
+import type { Widget } from "./typesLevel1";
+
+export const initialWidgets: Widget[] = [
+    { id: 1, content: "https://i.imgur.com/yxr4QMR.png" },
+    { id: 2, content: "https://i.imgur.com/2n11hJn.png" },
+    { id: 3, content: "https://i.imgur.com/DtUtPZK.png" },
+    { id: 4, content: "https://i.imgur.com/YmsczfQ.png" },
+    { id: 5, content: "https://i.imgur.com/hgPQ06G.png" },
+    { id: 6, content: "https://i.imgur.com/oRp6iv2.png" },
+    { id: 7, content: "https://i.imgur.com/BUGBEOc.png" },
+    { id: 8, content: "https://i.imgur.com/hNUaZS0.jpeg" },
+    { id: 9, content: "https://i.imgur.com/hNUaZS0.jpeg" },
+    { id: 10, content: "https://i.imgur.com/hNUaZS0.jpeg" },
+    { id: 11, content: "https://i.imgur.com/Now9q9e.png" },
+    { id: 12, content: "https://i.imgur.com/iuyJU46.png" },
+    { id: 13, content: "https://i.imgur.com/9as8FXr.png" },
+    { id: 14, content: "https://i.imgur.com/BUGBEOc.png" },
+    { id: 15, content: "https://i.imgur.com/BUGBEOc.png" },
+    { id: 16, content: "https://i.imgur.com/BUGBEOc.png" },
+];
