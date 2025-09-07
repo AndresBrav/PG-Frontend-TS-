9077775a77d444bf2b99e5ea2fa08fc866773d31
Andres Bravo <bravoandres1706@gmail.com>
2025-09-07 15:06:39 -0400

    graficos del ejercicio 2 implementados

--- src/pages/Games/Level1/widgetsDataLevel1.ts
+++ src/pages/Games/Level1/widgetsDataLevel1.ts
@@ -1,20 +1,49 @@
 import type { Widget } from "./typesLevel1";
 
+//Ejercicio 1
 export const initialWidgets: Widget[] = [
-    { id: 1, content: "https://i.imgur.com/yxr4QMR.png" }, /* desicion si no */
-    { id: 2, content: "https://i.imgur.com/2n11hJn.png" }, /* son iguales */
-    { id: 3, content: "https://i.imgur.com/DtUtPZK.png" }, /* leer n1,n2 */
-    { id: 4, content: "https://i.imgur.com/YmsczfQ.png" }, /* inicio */
-    { id: 5, content: "https://i.imgur.com/hgPQ06G.png" }, /* linea doblada */
-    { id: 6, content: "https://i.imgur.com/oRp6iv2.png" }, /* no son iguales */
-    { id: 7, content: "https://i.imgur.com/BUGBEOc.png" }, /* linea abajo  */
-    { id: 8, content: "https://i.imgur.com/hNUaZS0.jpeg" }, /* linea */
-    { id: 9, content: "https://i.imgur.com/hNUaZS0.jpeg" }, /* linea */
-    { id: 10, content: "https://i.imgur.com/hNUaZS0.jpeg" }, /* linea */
-    { id: 11, content: "https://i.imgur.com/Now9q9e.png" }, /* linea derecha */
-    { id: 12, content: "https://i.imgur.com/iuyJU46.png" }, /* linea izquierda */
-    { id: 13, content: "https://i.imgur.com/9as8FXr.png" }, /* fin */
-    { id: 14, content: "https://i.imgur.com/BUGBEOc.png" }, /* linea abajo */
-    { id: 15, content: "https://i.imgur.com/BUGBEOc.png" }, /* linea abajo */
-    { id: 16, content: "https://i.imgur.com/BUGBEOc.png" }, /* linea abajo */
+    { id: 1, content: "https://i.imgur.com/yxr4QMR.png" } /* desicion si no */,
+    { id: 2, content: "https://i.imgur.com/2n11hJn.png" } /* son iguales */,
+    { id: 3, content: "https://i.imgur.com/DtUtPZK.png" } /* leer n1,n2 */,
+    { id: 4, content: "https://i.imgur.com/YmsczfQ.png" } /* inicio */,
+    { id: 5, content: "https://i.imgur.com/hgPQ06G.png" } /* linea doblada */,
+    { id: 6, content: "https://i.imgur.com/oRp6iv2.png" } /* no son iguales */,
+    { id: 7, content: "https://i.imgur.com/BUGBEOc.png" } /* linea abajo  */,
+    { id: 8, content: "https://i.imgur.com/hNUaZS0.jpeg" } /* linea */,
+    { id: 9, content: "https://i.imgur.com/hNUaZS0.jpeg" } /* linea */,
+    { id: 10, content: "https://i.imgur.com/hNUaZS0.jpeg" } /* linea */,
+    { id: 11, content: "https://i.imgur.com/Now9q9e.png" } /* linea derecha */,
+    {
+        id: 12,
+        content: "https://i.imgur.com/iuyJU46.png",
+    } /* linea izquierda */,
+    { id: 13, content: "https://i.imgur.com/9as8FXr.png" } /* fin */,
+    { id: 14, content: "https://i.imgur.com/BUGBEOc.png" } /* linea abajo */,
+    { id: 15, content: "https://i.imgur.com/BUGBEOc.png" } /* linea abajo */,
+    { id: 16, content: "https://i.imgur.com/BUGBEOc.png" } /* linea abajo */,
+];
+
+// Ejercicio 2
+export const initialWidgetsEjercicio2: Widget[] = [
+    { id: 1, content: "https://i.imgur.com/8wCxpum.png" }, //primer descicion
+    { id: 2, content: "https://i.imgur.com/mt4Z8y1.png" }, //fin
+    { id: 3, content: "https://i.imgur.com/oC9J3TN.png" }, //flecha abajo 1
+    { id: 4, content: "https://i.imgur.com/3MzUgfA.png" }, //inicio
+    { id: 5, content: "https://i.imgur.com/r8r7BIt.png" }, //introdusca n1,n2
+    { id: 6, content: "https://i.imgur.com/v8uvFLP.png" }, //leer n1,n2
+    { id: 7, content: "https://i.imgur.com/L8yntMk.png" }, //descicion 2
+    { id: 8, content: "https://i.imgur.com/Od4jxIE.png" }, //esquina derecha 1
+    { id: 9, content: "https://i.imgur.com/MSjy0TA.png" }, //esquina derecha 2
+    { id: 10, content: "https://i.imgur.com/Y7okYCi.png" }, //esquina derecha 3
+    { id: 11, content: "https://i.imgur.com/42u2BvY.png" }, //esquina derecha 4
+    { id: 12, content: "https://i.imgur.com/cJqYyTX.png" }, //esquina izquierda 1
+    { id: 13, content: "https://i.imgur.com/jnKoRnc.png" }, //esquina izquierda 2
+    { id: 14, content: "https://i.imgur.com/L2Z7DVd.jpeg" }, //linea 1
+    { id: 15, content: "https://i.imgur.com/L2Z7DVd.jpeg" }, //linea 2
+    { id: 16, content: "https://i.imgur.com/L2Z7DVd.jpeg" }, //linea 3
+    { id: 17, content: "https://i.imgur.com/goRKMj3.png" }, //n1 es mayor
+    { id: 18, content: "https://i.imgur.com/sFAFzl4.png" }, //n2 es mayor
+    { id: 19, content: "https://i.imgur.com/oC9J3TN.png" }, //flecha abajo 2
+    { id: 20, content: "https://i.imgur.com/oC9J3TN.png" }, //flecha abajo 3
+    { id: 21, content: "https://i.imgur.com/oC9J3TN.png" }, //flecha abajo 4
 ];
