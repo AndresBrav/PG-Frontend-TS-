e766e44ff189ae40af630a150f5325e582fb15f4
Andres Bravo <bravoandres1706@gmail.com>
2025-09-24 16:50:20 -0400

    se agrego en vite.config.ts el server para permitir ngrok

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   base: './', // importante para que funcione en subcarpetas
+  server: {
+    allowedHosts: ["vanetta-thyrsoid-tanya.ngrok-free.dev"]
+  }
 })
