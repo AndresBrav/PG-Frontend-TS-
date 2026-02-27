5ef899b4fb2d8f8e5ec29e533d5ca414c0546c25
Andres Bravo <bravoandres1706@gmail.com>
2026-02-27 10:44:19 -0400

    otra parte añadida encabezado pseudocodigo

--- src/pages/Games/Presentation/Dashboard.tsx
+++ src/pages/Games/Presentation/Dashboard.tsx
@@ -445,6 +445,69 @@ const Dashboard = () => {
                     <div className="circle-inner">5</div>
                 </div>
             </div>
+            <br />
+
+            <div className="information-design-level-style-container-grid">
+                <div className="item-information-design-level-style-1">
+                    <h1 className="design-font-information-1">
+                        Etapa 2, Seccion 1
+                    </h1>
+                    <h1 className="design-font-information-2">Pseudocodigo</h1>
+                </div>
+                <div className="item-information-design-level-style-2">
+                    <svg
+                        className="item-information-design"
+                        // onClick={alternarModal}
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        style={{ cursor: "pointer" }}
+                    >
+                        <path
+                            d="M21 16C21 18.8284 21 20.2426 20.1213 21.1213C19.2426 22 17.8284 22 15 22H9C6.17157 22 4.75736 22 3.87868 21.1213C3 20.2426 3 18.8284 3 16V8C3 5.17157 3 3.75736 3.87868 2.87868C4.75736 2 6.17157 2 9 2H15C17.8284 2 19.2426 2 20.1213 2.87868C21 3.75736 21 5.17157 21 8V12"
+                            stroke="#000000"
+                            strokeWidth="1.5"
+                            strokeLinecap="round"
+                        />
+                        <path
+                            d="M8 2V6M8 22V10"
+                            stroke="#000000"
+                            strokeWidth="1.5"
+                            strokeLinecap="round"
+                        />
+                        <path
+                            d="M2 12H4"
+                            stroke="#000000"
+                            strokeWidth="1.5"
+                            strokeLinecap="round"
+                        />
+                        <path
+                            d="M2 16H4"
+                            stroke="#000000"
+                            strokeWidth="1.5"
+                            strokeLinecap="round"
+                        />
+                        <path
+                            d="M2 8H4"
+                            stroke="#000000"
+                            strokeWidth="1.5"
+                            strokeLinecap="round"
+                        />
+                        <path
+                            d="M11.5 6.5H16.5"
+                            stroke="#000000"
+                            strokeWidth="1.5"
+                            strokeLinecap="round"
+                        />
+                        <path
+                            d="M11.5 10H16.5"
+                            stroke="#000000"
+                            strokeWidth="1.5"
+                            strokeLinecap="round"
+                        />
+                    </svg>
+                </div>
+            </div>
         </>
     );
 };
