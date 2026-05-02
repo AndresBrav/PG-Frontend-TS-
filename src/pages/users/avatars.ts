7fdb4a43749c02cec9b6b2c75bdc2d10a7e1c0d5
Andres Bravo <andbrav1917@gmail.com>
2026-05-02 09:03:45 -0400

    refactor: images optimized

--- src/pages/users/avatars.ts
+++ src/pages/users/avatars.ts
@@ -1,14 +1,37 @@
+import avatar1 from '../../assets/avatares/woman1.webp';
+import avatar2 from '../../assets/avatares/grand master.webp';
+import avatar3 from '../../assets/avatares/man2.webp';
+import avatar4 from '../../assets/avatares/man3.webp';
+import avatar5 from '../../assets/avatares/man4.webp';
+import avatar6 from '../../assets/avatares/security man.webp';
+import avatar7 from '../../assets/avatares/woman2.webp';
+import avatar8 from '../../assets/avatares/woman3.webp';
+import avatar9 from '../../assets/avatares/man5.webp';
+import avatar10 from '../../assets/avatares/man6.webp';
+
 const avatars = [
-    { id: 1, url: "https://i.imgur.com/TxdbgOq.jpeg" },
-    { id: 2, url: "https://i.imgur.com/53Xfosm.png" },
-    { id: 3, url: "https://i.imgur.com/nlzFYjZ.png" },
-    { id: 4, url: "https://i.imgur.com/AEI3Wpz.png" },
-    { id: 5, url: "https://i.imgur.com/SKhtz4D.png" },
-    { id: 6, url: "https://i.imgur.com/4tT9t8y.png" },
-    { id: 7, url: "https://i.imgur.com/4lfaMNR.png" },
-    { id: 8, url: "https://i.imgur.com/pILnkcV.png" },
-    { id: 9, url: "https://i.imgur.com/NjKngjG.png" },
-    { id: 10, url: "https://i.imgur.com/mtWvbzJ.png" },
+    { id: 1, url: avatar1 },
+    { id: 2, url: avatar2 },
+    { id: 3, url: avatar3 },
+    { id: 4, url: avatar4 },
+    { id: 5, url: avatar5 },
+    { id: 6, url: avatar6 },
+    { id: 7, url: avatar7 },
+    { id: 8, url: avatar8 },
+    { id: 9, url: avatar9 },
+    { id: 10, url: avatar10 },
 ];
+// const avatars = [
+//     { id: 1, url: "https://i.imgur.com/TxdbgOq.jpeg" },
+//     { id: 2, url: "https://i.imgur.com/53Xfosm.png" },
+//     { id: 3, url: "https://i.imgur.com/nlzFYjZ.png" },
+//     { id: 4, url: "https://i.imgur.com/AEI3Wpz.png" },
+//     { id: 5, url: "https://i.imgur.com/SKhtz4D.png" },
+//     { id: 6, url: "https://i.imgur.com/4tT9t8y.png" },
+//     { id: 7, url: "https://i.imgur.com/4lfaMNR.png" },
+//     { id: 8, url: "https://i.imgur.com/pILnkcV.png" },
+//     { id: 9, url: "https://i.imgur.com/NjKngjG.png" },
+//     { id: 10, url: "https://i.imgur.com/mtWvbzJ.png" },
+// ];
 
 export default avatars;
