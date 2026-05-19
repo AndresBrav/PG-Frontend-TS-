c9745b5e9be9a1b2dd051b39563360e9aab54cbb
Andres Bravo <andbrav1917@gmail.com>
2026-05-18 21:55:31 -0400

    feat ejercicio Ia api added

---
+++ src/api/ejerciciosIA/ejercicioIAapi.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+const API = import.meta.env.VITE_API_URL;
+
+export const traerNotificaciones = async (clave: string): Promise<any> => {
+    try {
+        // Axios devuelve un objeto AxiosResponse, y el dato útil está en .data
+        const response = await axios.get(`${API}/juegosIA/traerJuegosConIA`, {
+            headers: {
+                Authorization: clave,
+                'ngrok-skip-browser-warning': 'true',
+            },
+        });
+
+        //
+        if (!response.data) return null;
+        console.log('el arreglo que se trae es ', response.data.juegosIA);
+        const { juegosIA, msg } = response.data;
+        return { juegosIA, msg };
+    } catch (error) {
+        console.error('Error fetching notifications:', error);
+        return null;
+    }
+};
